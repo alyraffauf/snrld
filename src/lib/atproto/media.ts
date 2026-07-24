c28ccea1bfd165805a188882be6e68818d0ec728
Aly Raffauf <aly@raffauflabs.com>
2026-07-24 13:24:27 -0400

    lib/atproto/media: use webp

--- src/lib/atproto/media.ts
+++ src/lib/atproto/media.ts
@@ -7,5 +7,5 @@ const DEFAULT_CDN = 'https://cdn.bsky.app'
 export function getAvatarUrl(did: Did, avatar: AvatarBlob, cdn = DEFAULT_CDN): string {
   const cid = 'ref' in avatar ? avatar.ref.$link : avatar.cid
 
-  return `${cdn}/img/avatar/plain/${did}/${cid}@jpeg`
+  return `${cdn}/img/avatar/plain/${did}/${cid}@webp`
 }
