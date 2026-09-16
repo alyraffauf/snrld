ae620d5abcbb590f4659b6f4f3f36c4f8d76c52f
Aly Raffauf <aly@aly.codes>
2026-09-15 23:39:40 -0400

    lib/microcosm: swap to slingshot.aly.town

--- src/lib/microcosm/client.ts
+++ src/lib/microcosm/client.ts
@@ -1,5 +1,5 @@
 import { Client, simpleFetchHandler } from '@atcute/client'
 
 export const slingshot = new Client({
-  handler: simpleFetchHandler({ service: 'https://slingshot.microcosm.blue' }),
+  handler: simpleFetchHandler({ service: 'https://slingshot.aly.town' }),
 })
